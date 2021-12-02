3eb266090ecbac5fd3a308222f2749b1ca43a268
moksi3 <simon.mok@oocl.com>
2021-12-02 14:04:44 +0800

    [fizz buzz][simon mok] feat: add multiple 3 test case

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -18,4 +18,17 @@ public class FizzBuzzTest {
 
     }
 
+    @Test
+    public void should_output_fizz_when_fizzbuzz_given_multiple_3()
+    {
+        int given = 6;
+        String expected = "Fizz";
+
+        FizzBuzz fb= new FizzBuzz();
+        String result = fb.countOff(given);
+
+        assertEquals(expected , result);
+
+    }
+
 }
